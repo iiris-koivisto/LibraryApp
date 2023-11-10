dfeb597f3b832423e837bfe5d83d4164444d626f
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-11-10 11:42:10 +0200

    Made user model

--- library/src/main/java/com/example/library/models/User.java
+++ library/src/main/java/com/example/library/models/User.java
@@ -1,6 +1,12 @@
 package com.example.library.models;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
@@ -18,5 +24,53 @@ public class User {
 		this.email = email;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPhoneNumber() {
+		return address;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+
+		return "User [id = " + id + ", name = " + name + ", email = " + email + ", "
+				+ "address = " + address + ", phone number = " + phoneNumber + "]";
+
 	}
 }
