8e5f1c55a8d31aef9db8c4347dbcfd5d7220d26d
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-11-13 11:59:49 +0200

    Shortened UserController

--- library/src/main/java/com/example/library/controllers/UserController.java
+++ library/src/main/java/com/example/library/controllers/UserController.java
@@ -9,64 +9,46 @@ import com.example.library.repositories.UserRepository;
 import java.util.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
-
-	private List<User> userList = new ArrayList <>();
 	
 	@GetMapping(value = "/users")
 	public List<User> getAllUsers() {
-		return userList;
+		return userRepository.findAll();
 	}
 
-	@GetMapping(value = "/users/{id}")
-	public User getUserById(@PathVariable(name = "id") int id) {
-		// Find the user with the given ID
-		for (User user : userList) {
-			if (user.getId() == id) {
-				return user;
-			}
-		}
-		// Return null if no user is found
-		return null;
-	}
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable int id) {
+	    // findById returns an Optional<User>
+        return userRepository.findById(id).orElse(null);
+    }
 
-	@PostMapping(value = "/users")
-	public User create(@RequestBody User user) {
-		// Assign a unique ID (you might want to use a more sophisticated ID generation strategy)
-        int nextId = userList.size() + 1;
-        user.setId(nextId);
+	@PostMapping
+	public User addUser(@RequestBody User user) {
+		  // Save the new user to the database
+        return userRepository.save(user);
+    }
 
-        // Add the new employee to the list
-        userList.add(user);
-
-        // Return the newly added employee
-        return user;
-	}
-
-	@PutMapping(value = "users/{id}")
+	@PutMapping("/{id}")
 	public User updateUser( @RequestBody User updatedUser,
 			@PathVariable int id) {
-		for (User user : userList) {
-			if (user.getId() == id) {
-				//Update user details
-				user.setName(updatedUser.getName());
-				user.setEmail(updatedUser.getEmail());
-				user.setAddress(updatedUser.getAddress());
-				user.setPhoneNumber(updatedUser.getPhoneNumber());
-				
-				//Return updated user
-				return userRepository.save(user);
-			}
-		}
-		//Return null if no user is found
-		return null;
-	}
+		// Check if the user with the given ID exists
+        return userRepository.findById(id)
+                .map(user -> {
+                    // Set the ID of the updated employee
+                    updatedUser.setId(id);
+                    // Save the updated employee to the database
+                    return userRepository.save(updatedUser);
+                })
+                .orElse(null);
+    }
 
-	@DeleteMapping(value = "/users/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteUser(@PathVariable int id) {
-		userList.removeIf(user -> user.getId() == id);
-	}
+	    // Delete the user with the given ID from the database
+        userRepository.deleteById(id);
+    }
 }
