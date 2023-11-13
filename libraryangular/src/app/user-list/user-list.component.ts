f6aefc65f9f0212a33422da050cd19ee07d6a6f7
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-11-13 12:55:05 +0200

    Added component User List

---
+++ libraryangular/src/app/user-list/user-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { UserService } from '../service/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+
+/*
+UserListComponent class uses the UserService’s findAll() method
+to fetch all the entities persisted in the database and stores 
+them in the users field.
+*/
+
+export class UserListComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.findAll().subscribe(data => {
+      this.users = data;
+    });
+  }
+}
