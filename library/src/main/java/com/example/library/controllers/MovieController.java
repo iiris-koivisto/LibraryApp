5e109f98ac0e0325e381eb6776efdc5849ce445b
hammywong <hammy.wong@edu.turkuamk.fi>
2023-11-13 10:36:09 +0200

    fixed errors in repository and controller of movie

--- library/src/main/java/com/example/library/controllers/MovieController.java
+++ library/src/main/java/com/example/library/controllers/MovieController.java
@@ -4,44 +4,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.library.models.Movie;
+import com.example.library.repositories.MovieRepository; // Make sure to import the correct repository.
 
-import java.util.*;
+import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
-	
-	@Autowired
-	private MovieRepository MovieRepository;
+    
+    @Autowired
+    private MovieRepository movieRepository;
 
-	@GetMapping(value = "/movies")
-	public List<Movie> getAll() {
-		return MovieRepository.findAll();
-	}
+    @GetMapping
+    public List<Movie> getAll() {
+        return movieRepository.findAll();
+    }
 
-	@GetMapping(value = "/movies/{id}")
-	public Movie get(@PathVariable(name = "id") Integer id) {
-		return MovieRepository.findById(id).get();
-	}
+    @GetMapping("/{id}")
+    public Movie get(@PathVariable Integer id) {
+        return movieRepository.findById(id).get();
+    }
 
-	@PostMapping(value = "/movies")
-	public Movie create(@RequestBody Movie movie) {
-		return MovieRepository.save(movie);
-	}
+    @PostMapping
+    public Movie create(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
 
-	@PutMapping(value = "/movies/{id}")
-	public Movie update( @RequestBody Movie movie,
-			@PathVariable(name = "id") Integer id) {
-		Movie m = movieRepository.findById(id).get();
-		m.setName(movie.getName());
-		m.setMovieStudio(movie.getMovieStudio());
-		m.setGenre(movie.getGenre());
-		return movieRepository.save(m);
-	}
+    @PutMapping("/{id}")
+    public Movie update(@RequestBody Movie movie, @PathVariable Integer id) {
+        Movie m = movieRepository.findById(id).get();
+        m.setName(movie.getName());
+        m.setMovieStudio(movie.getMovieStudio());
+        m.setGenre(movie.getGenre());
+        return movieRepository.save(m);
+    }
 
-	@DeleteMapping(value = "/users/{id}")
-	public Movie delete(@PathVariable(name = "id") Integer id) {
-		Movie m = movieRepository.findById(id).get();
-		movieRepository.delete(m);
-		return m;
-	}
+    @DeleteMapping("/{id}")
+    public Movie delete(@PathVariable Integer id) {
+        Movie m = movieRepository.findById(id).get();
+        movieRepository.delete(m);
+        return m;
+    }
 }
--- library/src/main/java/com/example/library/repositories/MovieRepository.java
+++ library/src/main/java/com/example/library/repositories/MovieRepository.java
@@ -3,5 +3,5 @@ package com.example.library.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.library.models.Movie;
 
-public interface MovieRepository extends JpaRepository<Movie, Integer>{
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
