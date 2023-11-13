68b9cf71732b96f9ef31e3e068780bee0e0e83db
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-11-13 10:49:37 +0200

    Change EmployeeController

--- library/src/main/java/com/example/library/controllers/EmployeeController.java
+++ library/src/main/java/com/example/library/controllers/EmployeeController.java
@@ -1,68 +1,53 @@
 package com.example.library.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.library.models.Employee;
+import com.example.library.repositories.EmployeeRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private List<Employee> employeeList = new ArrayList<>();
+	@Autowired
+    private EmployeeRepository employeeRepository;
 
     @GetMapping
     public List<Employee> getAllEmployees() {
-        return employeeList;
+        return employeeRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable int id) {
-        // Find the employee with the given ID
-        for (Employee employee : employeeList) {
-            if (employee.getId() == id) {
-                return employee;
-            }
-        }
-        // Return null if no employee found with the given ID
-        return null;
+        // findById returns an Optional<Employee>
+        return employeeRepository.findById(id).orElse(null);
     }
 
     @PostMapping
     public Employee addEmployee(@RequestBody Employee employee) {
-        // Assign a unique ID (you might want to use a more sophisticated ID generation strategy)
-        int nextId = employeeList.size() + 1;
-        employee.setId(nextId);
-
-        // Add the new employee to the list
-        employeeList.add(employee);
-
-        // Return the newly added employee
-        return employee;
+        // Save the new employee to the database
+        return employeeRepository.save(employee);
     }
 
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable int id, @RequestBody Employee updatedEmployee) {
-        // Find the employee with the given ID
-        for (Employee employee : employeeList) {
-            if (employee.getId() == id) {
-                // Update the employee details
-                employee.setName(updatedEmployee.getName());
-                employee.setUsername(updatedEmployee.getUsername());
-                employee.setPassword(updatedEmployee.getPassword());
-
-                // Return the updated employee
-                return employee;
-            }
-        }
-        // Return null if no employee found with the given ID
-        return null;
+        // Check if the employee with the given ID exists
+        return employeeRepository.findById(id)
+                .map(employee -> {
+                    // Set the ID of the updated employee
+                    updatedEmployee.setId(id);
+                    // Save the updated employee to the database
+                    return employeeRepository.save(updatedEmployee);
+                })
+                .orElse(null);
     }
 
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable int id) {
-        employeeList.removeIf(employee -> employee.getId() == id);
+        // Delete the employee with the given ID from the database
+        employeeRepository.deleteById(id);
     }
 }
