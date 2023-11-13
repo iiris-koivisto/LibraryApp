602aa28c08f2da8f0b8c8d98aead599d93c1d771
mamyllar <79257764+mamyllar@users.noreply.github.com>
2023-11-13 10:05:50 +0200

    Create Employee related files

--- library/src/main/java/com/example/library/models/Employee.java
+++ library/src/main/java/com/example/library/models/Employee.java
@@ -1,5 +1,70 @@
 package com.example.library.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+    private String name;
+    private String username;
+    private String password;
+    
+    public Employee() {
+    	super();
+    }
+
+    public Employee(int id, String name, String username, String password) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
+
