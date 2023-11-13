a43372b2dd18f8a6fc7b527e4c2e7776fc3932bd
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-11-13 10:54:10 +0200

    Fixed errors in user model, controller and repository

--- library/src/main/java/com/example/library/controllers/UserController.java
+++ library/src/main/java/com/example/library/controllers/UserController.java
@@ -3,43 +3,70 @@ package com.example.library.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.library.models.User;
+import com.example.library.repositories.UserRepository;
+
 import java.util.*;
 
 @RestController
-public class userController {
+public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
 
+	private List<User> userList = new ArrayList <>();
+	
 	@GetMapping(value = "/users")
-	public List<User> getAll() {
-		return userRepository.findAll();
+	public List<User> getAllUsers() {
+		return userList;
 	}
 
 	@GetMapping(value = "/users/{id}")
-	public User get(@PathVariable(name = "id") Integer id) {
-		return userRepository.findById(id).get();
+	public User getUserById(@PathVariable(name = "id") int id) {
+		// Find the user with the given ID
+		for (User user : userList) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		// Return null if no user is found
+		return null;
 	}
 
 	@PostMapping(value = "/users")
 	public User create(@RequestBody User user) {
-		return userRepository.save(user);
+		// Assign a unique ID (you might want to use a more sophisticated ID generation strategy)
+        int nextId = userList.size() + 1;
+        user.setId(nextId);
+
+        // Add the new employee to the list
+        userList.add(user);
+
+        // Return the newly added employee
+        return user;
 	}
 
-	@PutMapping(value = "/users/{id}")
-	public User update( @RequestBody User user,
-			@PathVariable(name = "id") Integer id) {
-		User u = userRepository.findById(id).get();
-		u.setFirstName(user.getFirstName());
-		u.setLastName(user.getLastName());
-		u.setEmail(user.getEmail());
-		return userRepository.save(u);
+	@PutMapping(value = "users/{id}")
+	public User updateUser( @RequestBody User updatedUser,
+			@PathVariable int id) {
+		for (User user : userList) {
+			if (user.getId() == id) {
+				//Update user details
+				user.setName(updatedUser.getName());
+				user.setEmail(updatedUser.getEmail());
+				user.setAddress(updatedUser.getAddress());
+				user.setPhoneNumber(updatedUser.getPhoneNumber());
+				
+				//Return updated user
+				return userRepository.save(user);
+			}
+		}
+		//Return null if no user is found
+		return null;
 	}
 
 	@DeleteMapping(value = "/users/{id}")
-	public User delete(@PathVariable(name = "id") Integer id) {
-		User u = userRepository.findById(id).get();
-		userRepository.delete(u);
-		return u;
+	public void deleteUser(@PathVariable int id) {
+		userList.removeIf(user -> user.getId() == id);
 	}
 }
