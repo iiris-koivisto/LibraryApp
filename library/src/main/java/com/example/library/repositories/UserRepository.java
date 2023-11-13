c8b27524c8ddfd49e818662570075580fc83083a
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-11-13 12:02:08 +0200

    Added tag @repository to UserRepository

--- library/src/main/java/com/example/library/repositories/UserRepository.java
+++ library/src/main/java/com/example/library/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.library.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.example.library.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 }
