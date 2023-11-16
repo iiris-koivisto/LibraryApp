028f2b490b6b82c488619da526a6352e2b8417c6
Iiris Koivisto <koivisto.irkku2gmail.com>
2023-11-16 11:30:40 +0200

    Added queries for LoanRepository

--- library/src/main/java/com/example/library/repositories/LoanRepository.java
+++ library/src/main/java/com/example/library/repositories/LoanRepository.java
@@ -1,9 +1,27 @@
 package com.example.library.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.library.models.Loan;
+import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Integer>{
+
+	@Query("SELECT l FROM Loans WHERE " +
+			"l.book_id = :book_id ")
+	List<Loan> getLoanByBookId(@Param("book_id")int book_id);
+	
+	
+	@Query("SELECT l FROM Loans WHERE " +
+			"l.movie_id = :movie_id ")
+	List<Loan> getLoanByMovieId(@Param("movie_id")int movie_id);
+	
+	
+	@Query("SELECT l FROM Loans WHERE " +
+			"l.user_id = :user_id " +
+			"AND isReturned = false")
+	List<Loan> getLoanByUserId(@Param("user_id")int user_id);
 }
